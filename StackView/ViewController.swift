d194078875f5833ccd1cdab4ccb623f595b3ce7b
AhmedAtef277 <91344134+AhmedAtef277@users.noreply.github.com>
2022-05-12 22:55:42 +0200

    stack view files

---
+++ StackView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by mac on 23/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
